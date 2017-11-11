ea48f4504e599a9de466c40349be826bcc131b5d
Todd Matthews <gtmatthews@gmail.com>
2017-11-11 17:02:14 -0500

    Add untracked files

---
+++ pages/index.vue
@@ -0,0 +1,207 @@
+<template>
+  <section class="container">
+    <div class="about nav-items">
+      <svg width="34px" height="32px" viewBox="0 0 34 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
+          <g id="home-page-open-menu" transform="translate(-17.000000, -261.000000)" stroke="#526488" stroke-width="2">
+            <g id="drawer" transform="translate(-17.000000, 194.000000)">
+              <g id="user" transform="translate(36.000000, 68.000000)">
+                <path d="M22,12 C22,15.05 20.3,17.75 18.5,19.4 L18.5,21 C18.5,22 19,23.55 24,24.5 C26.8792443,25.0510113 29.2011713,27.1794444 30,30 L0,30 C0.472563931,27.1812198 2.68121981,24.9725639 5.5,24.5 C10.5,23.6 11.5,22 11.5,21 L11.5,19.4 C9.7,17.75 8,15.1 8,12 L8,7.2 C8,2 11.7,0 15,0 C18.3,0 22,2 22,7.2 L22,12 Z" id="Shape"></path>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+      <span>About</span>
+    </div>
+    <div class="portfolio nav-items">
+      <svg width="36px" height="36px" viewBox="0 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+            <g id="home-page-open-menu" transform="translate(-18.000000, -374.000000)" stroke-width="2">
+                <g id="drawer" transform="translate(-17.000000, 194.000000)">
+                    <g id="work-bag" transform="translate(36.000000, 181.000000)">
+                        <path d="M7.93333333,6.8 L7.93333333,3.4 C7.93333333,1.52223185 9.45556518,2.51650552e-16 11.3333333,0 L14.7333333,0 C16.6111015,2.51650552e-16 18.1333333,1.52223185 18.1333333,3.4 L18.1333333,6.8 M20.4,4.4408921e-16 L21.5333333,4.4408921e-16 C23.4111015,4.4408921e-16 24.9333333,1.52223185 24.9333333,3.4 L24.9333333,6.8" id="Shape" stroke="#414F6C"></path>
+                        <path d="M0,6.8 L34,6.8 L34,34 L0,34 L0,6.8 Z M24.9333333,34 L24.9333333,6.8 L24.9333333,34 Z M29.4666667,6.8 L29.4666667,29.4666667 L29.4666667,6.8 Z M29.4666667,29.4666667 L34,34 L29.4666667,29.4666667 Z" id="Shape" stroke="#526488"></path>
+                    </g>
+                </g>
+            </g>
+        </g>
+      </svg>
+      <span>Portfolio</span>
+    </div> <!-- porfolio -->
+    <div class="resume nav-items">
+      <svg width="36px" height="36px" viewBox="0 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+          <g id="home-page-open-menu" transform="translate(-16.000000, -504.000000)" stroke-width="2">
+              <g id="drawer" transform="translate(-17.000000, 194.000000)">
+                  <g id="resume" transform="translate(34.000000, 311.000000)">
+                      <path d="M27.2,24.9333333 L27.2,4.53333333 C27.2,2.02964247 25.1703575,2.51650552e-16 22.6666667,0 L4.08,0 C6.57333333,0 7.93333333,2.04 7.93333333,4.53333333 L7.93333333,29.4666667 C7.93333333,31.9703575 9.9629758,34 12.4666667,34" id="Shape" stroke="#526488"></path>
+                      <path d="M29.4666667,24.9333333 L12.4666667,24.9333333 C14.9703575,24.9333333 17,26.9629758 17,29.4666667 C17,31.9703575 14.9703575,34 12.4666667,34 L29.4666667,34 C31.9703575,34 34,31.9703575 34,29.4666667 C34,26.9629758 31.9703575,24.9333333 29.4666667,24.9333333 Z M4.08,0 C1.58666667,0 0,2.04 0,4.53333333 L7.93333333,4.53333333 L4.08,0 Z" id="Shape" stroke="#526488"></path>
+                      <path d="M13.6,6.8 L21.5333333,6.8 M13.6,12.4666667 L21.5333333,12.4666667 M13.6,18.1333333 L21.5333333,18.1333333" id="Shape" stroke="#414F6C"></path>
+                  </g>
+              </g>
+          </g>
+        </g>
+      </svg>
+      <span>Resume</span>
+    </div>
+    <div class="contact nav-items">
+      <svg width="36px" height="36px" viewBox="0 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+          <g id="home-page-open-menu" transform="translate(-16.000000, -634.000000)" stroke-width="2">
+            <g id="drawer" transform="translate(-17.000000, 194.000000)">
+              <g id="read-email" transform="translate(34.000000, 441.000000)">
+                <path d="M4.53333333,11 L0,15 M29.4666667,11 L34,15" id="Shape" stroke="#526488"></path>
+                <path d="M30,18 L30,0 L5,0 L5,18 M9.54545455,5.69620253 L15.2272727,5.69620253 M12.9545455,10.2531646 L25.4545455,10.2531646 M9.54545455,14.8101266 L25.4545455,14.8101266" id="Shape" stroke="#414F6C"></path>
+                <path d="M34,15 L34,34 L0,34 L0,15 M22.7233333,22.7676471 L34,15 M0,15 L11.2766667,22.7676471" id="Shape" stroke="#526488"></path>
+                <path d="M0,34 L11.8433333,21.7044745 C14.0533333,19.4318418 19.89,19.4318418 22.1,21.7044745 L34,34" id="Shape" stroke="#526488"></path>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+      <span>Contact</span>
+    </div> <!--contact -->
+    <div class="name">
+      <img class="profile-pic" src="~/assets/profile-pic-optimized.png" alt="profile-picture">
+      <h1 class="title">Todd Matthews</h1>
+      <h2 class="subtitle">Web Developer</h2>
+    </div> <!-- name -->
+    <div class="bio">
+      <p>I am a classical musician turned coder that is obsessed with learning and building. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eleifend in tortor ac congue. Proin et metus interdum, pretium arcu a, venenatis felis. Nulla tempus augue a neque gravida euismod. Sed fermentum ante magna, at consdf.</p>
+      <button class="button--purple">
+        <a class="button-link" href="/porfolio" >
+          See My Work
+          <svg class="circle-arrow" width="21px" height="21px" viewBox="0 0 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
+                  <g id="home-page-open-menu" transform="translate(-533.000000, -741.000000)" stroke-width="2" stroke="#E0DEF0">
+                      <g id="Button-See-My-Work" transform="translate(315.000000, 712.000000)">
+                          <g id="arrow-right-circle" transform="translate(219.000000, 30.000000)">
+                              <g id="Group-2">
+                                  <circle id="Oval" cx="9.5" cy="9.5" r="9.5"></circle>
+                                  <path d="M3,9.50063636 L14,9.50063636 M9.91365714,6 L13.9993714,9.5 L9.91365714,13" id="Shape"></path>
+                              </g>
+                          </g>
+                      </g>
+                  </g>
+              </g>
+          </svg>      
+        </a>
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: true,
+      navItems: ['About', 'Portfolio', 'Resume', 'Contact']
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  height: 100vh; 
+  display: grid;
+  grid-template-columns: 10% 1fr 1fr 1fr 1fr 10%;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 5px;
+  background-color: #1f1f1f;
+}
+
+.name {
+  grid-column: 2/6;
+  grid-row: 2/3;
+}
+
+.bio {
+  grid-column: 2/6;
+  grid-row: 3/6;
+  
+}
+
+.title {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  color: #526488;
+  font-size: 5rem;
+  line-height: 4rem;
+}
+
+.subtitle {
+  color: #35495e;
+  font-weight: 300;
+  font-size: 4rem;
+  word-spacing: 5px;
+  padding-left: 155px;
+}
+
+.bio {
+font-family: Quicksand-Regular;
+font-size: 1.25rem;
+color: #8C8C8C;
+letter-spacing: 0.75px;
+margin: 140px 0 10px 0;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+a.button-link {
+font-family: Quicksand-Bold;
+font-size: 1.3rem;
+color: #E0DEF0;
+letter-spacing: 0.76px;
+}
+
+a {
+  text-decoration: none;
+}
+
+svg.circle-arrow {
+  padding-top: 5px;
+}
+
+img.profile-pic {
+  float: left;
+}
+
+.nav-items {
+  align-self: center;
+  color: #526488;
+  font-family: "Source Sans Pro";
+  font-size: 2rem;
+  padding-bottom: 40px;
+}
+
+.about {
+  grid-column: 2/3;
+  grid-row: 1/2;
+  padding-bottom: 35px;
+}
+
+.portfolio {
+  grid-column: 3/4;
+   grid-row: 1/2;
+}
+
+.resume {
+  grid-column: 4/5;
+   grid-row: 1/2;
+}
+
+.contact {
+  grid-column: 5/6;
+  grid-row: 1/2;
+}
+
+.nav-items svg {
+  margin-right: 10px;
+}
+</style>
